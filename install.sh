ebbb51151463fb302fd39ca8c77d72477e531706
www <git@w3lab.kr>
2020-09-27 14:55:25 +0900

    Build amp directure

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
 # Download and run the latest release version.
-# https://github.com/username/repository
+# https://github.com/w3labkr/sh-amp-init
 #
 # Usage
-# git clone https://github.com/username/repository.git
-# cd repository
+# git clone https://github.com/w3labkr/sh-amp-init.git
+# cd sh-amp-init
 # chmod +x ./install.sh
 # ./install.sh
 
@@ -30,10 +30,10 @@ find ./ -type f -name "*.sh" -exec chmod +x {} +
 
 # Print a welcome message.
 echo
-echo "Start installing repository installaion."
+echo "Installation begins."
 
 # ...
 
 # Print a completion message.
 echo
-echo "repository is completely installed."
+echo "Installation is complete."
---
+++ ubuntu/20.04/pkg/vsftpd/uninstall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Download and run the latest release version.
+# https://github.com/w3labkr/sh-amp-init
+#
+# Usage
+# git clone https://github.com/w3labkr/sh-amp-init.git
+# cd sh-amp-init
+# chmod +x ./install.sh
+# ./install.sh
+
+# Work even if somebody does "sh thisscript.sh".
+set -e
+
+# Print a welcome message.
+echo
+echo "Uninstallation begins."
+
+# ...
+
+# Print a completion message.
+echo
+echo "Removal is complete."
