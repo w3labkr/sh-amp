950a5a6e3add40a93e3454b09149a2e09daac781
W3Labkr <git@w3lab.kr>
2020-09-27 18:44:31 +0900

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -33,17 +33,13 @@ fi
 # https://askubuntu.com/questions/459402/how-to-know-if-the-running-platform-is-ubuntu-or-centos-with-help-of-a-bash-scri
 OS_NAME="$(cat /etc/os-release | awk -F '=' '/^NAME=/{print $2}' | awk '{print $1}' | tr -d '"')"
 
-if [ "${OS_NAME}" == "Ubuntu" ]; then
-  OS_ID="ubuntu"
+if [ "${OS_NAME,,}" == "ubuntu" ]; then
   OS_VERSION_ID="$(cat /etc/os-release | awk -F '=' '/^VERSION_ID=/{print $2}' | awk '{print $1}' | tr -d '"')"
-  OS_VERSION_NUMBER="${OS_VERSION_ID//./}"
-  if [ "${OS_VERSION_NUMBER}" -lt "1804" ]; then
+  if [ "${OS_VERSION_ID//./}" -lt "1804" ]; then
     echo "Sorry. Not supported on Ubuntu below 18.04."
     exit 0
-  else
-    OS_VERSION_ID="18.04"
   fi
-elif [ "${OS_NAME}" == "CentOS" ]; then
+elif [ "${OS_NAME,,}" == "centos" ]; then
   echo "Sorry. Not supported on CentOS."
   exit 0
 else
@@ -65,7 +61,61 @@ echo '      \/  \/ \___|_|\___\___/|_| |_| |_|\___| (_)  '
 echo '                                                   '
 echo '---------------------------------------------------'
 
-#...
+# Set the arguments.
+OS_VERSION_ID="20.04"
+OS_DIR="${OS_NAME,,}/${OS_VERSION_ID}/os"
+PKG_DIR="${OS_NAME,,}/${OS_VERSION_ID}/pkg"
+FILE="$(basename $0)"
+
+if [ "${#@}" -eq "0" ]; then
+  # Run the operating system installation script.
+  bash "${OS_DIR}/${FILE}"
+
+  # Run the package installation script.
+  PACKAGES=('apache2' 'cerbot' 'fail2ban' 'mariadb' 'php' 'sendmail' 'vsftpd' 'wp-cli')
+  for ((i = 0; i < ${#PACKAGES[@]}; i++)); do
+    FILEPATH="${PKG_DIR}/${PACKAGES[$i]}/${FILE}"
+    if [ ! -f "${FILEPATH}" ]; then
+      echo "${FILEPATH} does not exist."
+      exit 0
+    fi
+    bash "${FILEPATH}"
+  done
+
+  # Print a completion message.
+  echo
+  echo "Installation is complete."
+  echo
+  echo '-------------------------------------------------------------------------------'
+  echo '   _    _                                 _                _               _   '
+  echo '  | |  | |                               (_)              | |             | |  '
+  echo '  | |__| | __ ___   _____    __ _   _ __  _  ___ ___    __| | __ _ _   _  | |  '
+  echo '  |  __  |/ _  \ \ / / _ \  / _  | |  _ \| |/ __/ _ \  / _  |/ _  | | | | | |  '
+  echo '  | |  | | (_| |\ V /  __/ | (_| | | | | | | (_|  __/ | (_| | (_| | |_| | |_|  '
+  echo '  |_|  |_|\__,_| \_/ \___|  \__,_| |_| |_|_|\___\___|  \__,_|\__,_|\__, | (_)  '
+  echo '                                                                    __/ |      '
+  echo '                                                                   |___/       '
+  echo '-------------------------------------------------------------------------------'
+  exit 0
+fi
+
+# It runs only if there are parameters.
+for arg in "${@}"; do
+  case "$arg" in
+  --[oO][sS])
+    bash "${OS_DIR}/${FILE}"
+    ;;
+  --*)
+    PACKAGE="$(echo "$arg" | sed -E 's/--//')"
+    FILEPATH="${PKG_DIR}/${PACKAGE,,}/${FILE}"
+    if [ ! -f "${FILEPATH}" ]; then
+      echo "${FILEPATH} does not exist."
+      exit 0
+    fi
+    bash "${FILEPATH}"
+    ;;
+  esac
+done
 
 # Print a completion message.
 echo
